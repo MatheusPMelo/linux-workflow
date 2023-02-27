5019985c28b53314b13cb1c727a4d3b1c8942604
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-02-27 18:05:43 -0300

    incerção de tratativas de erros

--- softwares.py
+++ softwares.py
@@ -6,7 +6,7 @@ def installGit() :
     try :
         os.system("sudo apt-get install git -y")
     except Exception as e :
-        print(f"Opss! Houve um erro: na instalação do git: {e}")
+        print(f"Opss! Houve um erro na instalação do git: {e}")
         next = input("Proceguir? [s/n]")
 
         if next == 'N' or next == 'n' :
@@ -14,79 +14,198 @@ def installGit() :
 
 def installPacSnap() :
     clear()
-    os.system("sudo apt-get install snapd -y")
+    try :
+        os.system("sudo apt-get install snapd -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
 
 def installVim() :
     clear()
-    os.system("sudo apt-get install vim -y")
+    try :
+        os.system("sudo apt-get install vim -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installNeovim() :
     clear()
-    os.system("cd ~")
-    os.system("wget -O neovim.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb")
-    os.system("sudo apt install ./neovim.deb")
-    os.system("rm -rf ~/neovim.deb")
+    try :
+        os.system("cd ~")
+        os.system("wget -O neovim.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb")
+        os.system("sudo apt install ./neovim.deb")
+        os.system("rm -rf ~/neovim.deb")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
+
 
 def installCurl() :
     clear()
-    os.system("sudo apt-get install curl -y")
+    try:
+        os.system("sudo apt-get install curl -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installNode() :
     clear()
-    os.system("sudo apt-get install nodejs -y")
+    try:
+        os.system("sudo apt-get install nodejs -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installMake() :
     clear()
-    os.system("sudo apt-get install make -y")
+    try :
+        os.system("sudo apt-get install make -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installWget() :
     clear()
-    os.system("sudo apt-get install wget -y")
+    try :
+        os.system("sudo apt-get install wget -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installGimp() :
     clear()
-    os.system("sudo apt-get install gimp -y")
+    try :
+        os.system("sudo apt-get install gimp -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installSpotify() :
     clear()
-    os.system("sudo apt-get install spotify -y")
+    try:
+        os.system("sudo apt-get install spotify -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installBrave() :
     clear()
-    os.system("sudo apt install apt-transport-https curl gnupg -y")
-    os.system("curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -")
-    os.system("sudo apt-get update")
-    os.system("sudo apt install brave-browser -y")
+    try :
+        os.system("sudo apt install apt-transport-https curl gnupg -y")
+        os.system("curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -")
+        os.system("sudo apt-get update")
+        os.system("sudo apt install brave-browser -y")
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installFlatRemix() :
     clear()
-    os.system('sudo add-apt-repository ppa:daniruiz/flat-remix -y')
-    os.system('sudo apt update -y')
-    os.system('sudo apt install flat-remix-gnome -y')
-    os.system('gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-Violet-Dark"')
+    try :
+        os.system('sudo add-apt-repository ppa:daniruiz/flat-remix -y')
+        os.system('sudo apt update -y')
+        os.system('sudo apt install flat-remix-gnome -y')
+        os.system('gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-Violet-Dark"')
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installFlatRemixGTK() : 
     clear()
-    os.system('sudo add-apt-repository ppa:daniruiz/flat-remix -y && sudo apt-get update && sudo apt-get install flat-remix-gtk -y')
-    os.system('gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Violet-Dark"')
+    try :
+        os.system('sudo add-apt-repository ppa:daniruiz/flat-remix -y && sudo apt-get update && sudo apt-get install flat-remix-gtk -y')
+        os.system('gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Violet-Dark"')
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installFlatRemixIcons() :
     clear()
-    os.system('cd ~')
-    os.system('wget -O flat-remix-master.zip https://github.com/daniruiz/flat-remix/archive/master.zip')
-    os.system('unzip flat-remix-master.zip')
-    os.system('cd flat-remix-master')
-    os.system('mv * ~/.local/share/icons/')
-    os.system('gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Violet-Dark"')
-    os.system('rm -rf flat-remix-master')
+    try :
+        os.system('cd ~')
+        os.system('wget -O flat-remix-master.zip https://github.com/daniruiz/flat-remix/archive/master.zip')
+        os.system('unzip flat-remix-master.zip')
+        os.system('cd flat-remix-master')
+        os.system('mv * ~/.local/share/icons/')
+        os.system('gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Violet-Dark"')
+        os.system('rm -rf flat-remix-master')
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installFlatRemixLockScreen() :
     clear()
-    os.system('sudo apt install libglib2.0-dev-bin imagemagick -y')
-    os.system('git clone https://github.com/daniruiz/flat-remix-gnome')
-    os.system('cd flat-remix-gnome')
-    os.system('make && sudo make install -y')
+    try :
+        os.system('sudo apt install libglib2.0-dev-bin imagemagick -y')
+        os.system('git clone https://github.com/daniruiz/flat-remix-gnome')
+        os.system('cd flat-remix-gnome')
+        os.system('make && sudo make install -y')
+    except Exception as e :
+        print(f"Opss! Houve um erro na instalação: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
+
 
 def installLunarVim() :
     clear()
-    os.system("LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)")
+    try :
+        os.system("LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)")
+    except Exception as e :
+        print(f"Opss! Houve um problema na instalação do lunarvim: {e}")
+        next = input("Proceguir? [s/n]")
+
+        if next == 'N' or next == 'n' :
+            return exit()
